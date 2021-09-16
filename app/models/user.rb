4e52ff8760fad1083ba4be60153c118ed1fd47b7
Marco D <mcruz@geosatelital.com.pe>
2021-09-16 08:41:38 -0500

    Se implemento validaciones de usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
-    
+
+    validates :username, uniqueness: { case_sensitive: false }, 
+                        length: { minimum: 3, maximum: 25 }
+
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+    validates :email, length: { maximum: 105 },
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX }
 end
